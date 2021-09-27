e9a36f3bf7448cbc9ec4069037112b5cf9a3a2e2
shadowwhite2010 <70062611+shadowwhite2010@users.noreply.github.com>
2021-09-28 01:04:11 +0530

    React framework with basic components added

---
+++ frontend/src/App.js
@@ -0,0 +1,33 @@
+// import logo from './logo.svg';
+import './App.css';
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route
+} from "react-router-dom";
+import Navbar from './components/Navbar';
+import About from './components/About';
+import Home from './components/Home';
+
+
+function App() {
+  return (
+    <>
+      <Router>
+        <Navbar title = "EventMania"/>
+        <div>
+          <Switch>
+            <Route exact path="/">
+              <Home/>
+            </Route>
+            <Route exact path = "/about">
+              <About/>
+            </Route>
+          </Switch>
+        </div>
+      </Router>
+    </>
+  );
+}
+
+export default App;
